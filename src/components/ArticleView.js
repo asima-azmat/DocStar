ed883dd4692b2d804d59c46d0b02e955212d9623
Farrukh Mushtaq <ga53soc@mytum.de>
2018-06-27 08:56:32 +0200

    First draft of ArticleView

--- src/components/ArticleView.js
+++ src/components/ArticleView.js
@@ -5,8 +5,9 @@ import {
     clap,
     follow
 } from './../redux/actions/actions'
+import {Badge} from 'react-materialize'
+import {Panel,Label} from 'react-bootstrap'
 import PropTypes from 'prop-types'
-import AsideFeed from './AsideFeed'
 import FollowButton from './FollowButton'
 import AddCommentButton from './AddCommentButton'
 import Editor from './Editor'
@@ -34,19 +35,22 @@ class Popup extends React.Component {
 
 
 
-class ArticleView extends Component {
+class ArticleView extends Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+          showPopup: false
+        };
+    }
 
-    constructor() {
-    super();
-    this.state = {
-      showPopup: false
-    };
-  }
-  togglePopup() {
-    this.setState({
-      showPopup: !this.state.showPopup
-    });
-  }
+    togglePopup()
+    {
+        this.setState({
+          showPopup: !this.state.showPopup
+        });
+    }
 
 
 
@@ -59,179 +63,149 @@ class ArticleView extends Component {
     componentWillUnmount() {
         document.body.className = ''
     }
-    render() {
-                    console.log(this.props._article);
 
-        const { text, _id, blogText, feature_img, doctorId ,comments} = this.props._article
+    static getCreatedDate(date)
+    {
+        date = new Date(date);
+        return date.toDateString();
+    }
 
+    static getCommentCount(comments)
+    {
+        if (Array.isArray(comments))
+            return comments.length;
+    }
 
+    static giveName(person)
+    {
+        return person.firstName + ' ' + person.lastName
+    }
+
+    render()
+    {
+        const { _id, blogText, feature_img, doctorId ,comments, blogHeading, createdAt} = this.props._article
 
         let author_name, author_img, author_id
-        if (doctorId) {
+        if (doctorId)
+        {
             const { firstName, lastName, _id } = doctorId
-            author_name = firstName
-            author_id = lastName
+            author_name = firstName + ' ' + lastName
+            author_id = _id
             author_img = _id
         }
 
+        const CommentsVar = ({comments = []}) => (
+          <div>
+            {comments.map(comment => (
+                <Panel>
+                    <Panel.Heading>{ArticleView.giveName(comment.personId)} wrote:</Panel.Heading>
+                    <Panel.Body>{comment.commentText}</Panel.Body>
+                </Panel>
+            ))}
+          </div>
+        )
 
-const Test = ({comments = []}) => (
-  <div>
-    {comments.map(station => (
-      <div className="comments" key={station.commentText}>{station.commentText}</div>
-    ))}
-  </div>
-); 
-
-
-
-
-
-
-
-        return ( 
-                <div>
+        return (
+            <div>
                 <div className="container-fluid main-container">
-                <div className="row animated fadeInUp" data-animation="fadeInUp-fadeOutDown">
-                    <div id="main-post" className="col-xs-10 col-md-8 col-md-offset-2 col-xs-offset-1 main-content">
-                        <div className="pull-right">
-                            {this.props.user ? <FollowButton user={`${this.props.user.following}`} to_follow={`${author_id}`} /> : ''}
-                        </div>
-                        <div className="post-metadata">
-                            <img alt={author_name} className="avatar-image" src={author_img} height="40" width="40" />
-                            <div className="post-info">
-                                <div data-react-className="PopoverLink" data-react-props=""><span className="popover-link" data-reactroot=""><a href={`/profile/${author_id}`}>{author_name}</a></span></div>
-                                <small>Published • nice story</small>
-                            </div>
-                        </div>
-
-                        {!feature_img || !feature_img.length > 0 ? '' : <div className="post-picture-wrapper">
-                            <img src={feature_img} alt="feature img 540" />
-                        </div> }
-                        <h3 className="blogText">{author_name}</h3>
-                        <div className="body">
-                            <p></p>
-                            <p className=""dangerouslySetInnerHTML={{__html: text}}>
-                            </p>
-                            <p></p>
-                        </div>
-                        <div className="post-tags">
-                            <a className="tag" href="">Story</a>
-                            <a className="tag" href="">Community</a>
-                        </div>
-                        <div className="post-stats clearfix">
-                            <div className="pull-left">
-                                <div className="like-button-wrapper">
-                                    <button onClick={() => this.props.clap(this.props._article._id)} className="like-button" data-behavior="trigger-overlay" type="submit">
-                                    <i className="fa fa-heart-o"></i><span className="hide-text">Like</span>
-                                    </button>
-                                     <span className="like-count">{_id}</span>
-                                </div>
-                            </div>
-                            <div className="pull-left">
-                                <a className="response-icon-wrapper" href="#">
-                                    <i className="fa fa-comment-o"></i>
-                                    <span className="response-count" data-behavior="response-count">0</span>
-                                </a>
-                            </div>
+                    <div className="row animated fadeInUp" data-animation="fadeInUp-fadeOutDown">
+                        <div id="main-post" className="col-xs-10 col-md-8 col-md-offset-2 col-xs-offset-1 main-content">
                             <div className="pull-right">
-                                <div className="bookmark-button-wrapper">
-                                    <form className="button_to" method="get" action=""><button className="bookmark-button" data-behavior="trigger-overlay" type="submit">      <span className="icon-bookmark-o"></span><span className="hide-text">Bookmark</span></button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="author-isnfo">
-                            <div clas="author-metasdata">
-                                <div className="usernasme-description">
-                                    <h4>{blogText}</h4>
-                                    <p></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div>
-    
 
-                <div className="post-show-footer row animated fadeInUp" data-animation="fadeInUp-fadeOutDown">
-                    <div className="col-xs-10 col-md-6 col-xs-offset-1 col-md-offset-3 main-content related-stories">
-                        <h4 className="small-heading">Related stories</h4>
-                        <div className="post-list-item">
-                            <div className="flex-container">
-                                <div className="avatar-wrapper">
-                                    <img alt="" className="avatar-image" src="" height="40" width="40" />
-                                </div>
+                            </div>
+                            <div className="post-metadata">
+                                <img alt='' className="avatar-image" src={author_img} height="40" width="40" />
                                 <div className="post-info">
-                                    <strong className="pli-title"><a href="#"></a></strong><br/>
-                                    <small className="pli-username"><a href="#"></a></small>
+                                    <div data-react-className="PopoverLink" data-react-props=""><span className="popover-link" data-reactroot=""><a href={`/doctor/${author_id}`}>{author_name}  </a></span>
+                                        <Label bsStyle="primary">Primary</Label>
+                                    </div>
+                                    <small>Published on {ArticleView.getCreatedDate(createdAt)}</small>
+                                </div>
+                            </div>
+
+                            {/*If blog has a feature image*/}
+                            {!feature_img || !feature_img.length > 0 ? '' : <div className="post-picture-wrapper">
+                                <img src={feature_img} alt="feature img 540" />
+                            </div> }
+
+                            <h3 className="blogText">{blogHeading}</h3>
+                            <div className="body">
+                                <p></p>
+                                <p className=""dangerouslySetInnerHTML={{__html: blogText}}>
+                                </p>
+                                <p></p>
+                            </div>
+
+                            {/*<div className="post-tags">
+                                <a className="tag" href="">Story</a>
+                                <a className="tag" href="">Community</a>
+                            </div>*/}
+                        </div>
+                    </div>
+                    <div>
+                        <div className="post-show-footer row animated fadeInUp" data-animation="fadeInUp-fadeOutDown">
+                            <div id="responses" className="col-xs-10 col-md-6 col-xs-offset-1 col-md-offset-3 main-content">
+                                <h4 className="medium-heading"><Badge>{ArticleView.getCommentCount(comments)}</Badge> Responses</h4>
+                                <CommentsVar comments={comments}></CommentsVar>
+                            </div>
+                            {/*<div className="col-xs-10 col-md-6 col-xs-offset-1 col-md-offset-3 main-content related-stories">
+                                <h4 className="small-heading">Related stories</h4>
+                                <div className="post-list-item">
+                                    <div className="flex-container">
+                                        <div className="avatar-wrapper">
+                                            <img alt="" className="avatar-image" src="" height="40" width="40" />
+                                        </div>
+                                        <div className="post-info">
+                                            <strong className="pli-title"><a href="#">asdasd</a></strong><br/>
+                                            <small className="pli-username"><a href="#">asdad</a></small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>*/}
+                        </div>
+                    </div>
+                    {/*<div className="post-metadata-bar" data-page="post-metadata-bar">
+                        <div className="flex-container is-inView" data-behavior="animated-metadata">
+                            <div className="post-stats flex-container">
+                                <div className="like-button-wrapper">
+                                    <form className="button_to" method="get" action=""><button className="like-button" data-behavior="trigger-overlay" type="submit">      <i className="fa fa-heart-o"></i><span className="hide-text">Like</span></button>
+                                    </form> <span className="like-count">0</span>
+                                </div>
+
+                                <div>
+                                    <a className="response-icon-wrapper" href="https://my-medium-clone.herokuapp.com/posts/it-s-looking-good#responses">
+                                        <i className="fa fa-comment-o"></i>
+                                        <span className="response-count" data-behavior="response-count">asdas</span>
+                                    </a>
+                                </div>
+                                <div className="bookmark-button">
+                                    <div className="bookmark-button-wrapper">
+                                        <form className="button_to" method="get" action=""><button className="bookmark-button" data-behavior="trigger-overlay" type="submit">      <span className="icon-bookmark-o"></span><span className="hide-text">Bookmark</span></button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="metabar-author-info flex-container flex-space-btw">
+                                <div>
+                                    <img alt={author_name} className="avatar-image" src={author_img} height="35" width="35" />
+                                    <div data-react-className="PopoverLink" ><span className="popover-link" data-reactroot=""><a href={`/profile/${author_img}`}>{author_name}</a></span></div>
+                                </div>
+
+                                <div className="container-fluid main-container">
+                                    <div className="col-md-6 col-md-offset-1 dashboard-main-content">
+                                        <div className="posts-wrapper animated fadeInUp" data-behavior="endless-scroll" data-animation="fadeInUp-fadeOutDown">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div id="responses" className="col-xs-10 col-md-6 col-xs-offset-1 col-md-offset-3 main-content">
-                        <h4 className="small-heading">Responses</h4>
-                        <Test comments={comments} />
-  </div>
-                    </div>
+                    </div>*/}
                 </div>
-                <div className="post-metadata-bar" data-page="post-metadata-bar">
-                    <div className="flex-container is-inView" data-behavior="animated-metadata">
-                        <div className="post-stats flex-container">
-                            <div className="like-button-wrapper">
-                                <form className="button_to" method="get" action=""><button className="like-button" data-behavior="trigger-overlay" type="submit">      <i className="fa fa-heart-o"></i><span className="hide-text">Like</span></button>
-                                </form> <span className="like-count">0</span>
-                            </div>
-
-                            <div>
-                                <a className="response-icon-wrapper" href="https://my-medium-clone.herokuapp.com/posts/it-s-looking-good#responses">
-                                    <i className="fa fa-comment-o"></i>
-                                    <span className="response-count" data-behavior="response-count">0</span>
-                                </a>
-                            </div>
-                            <div className="bookmark-button">
-                                <div className="bookmark-button-wrapper">
-                                    <form className="button_to" method="get" action=""><button className="bookmark-button" data-behavior="trigger-overlay" type="submit">      <span className="icon-bookmark-o"></span><span className="hide-text">Bookmark</span></button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="metabar-author-info flex-container flex-space-btw">
-                            <div>
-                                <img alt={author_name} className="avatar-image" src={author_img} height="35" width="35" />
-                                <div data-react-className="PopoverLink" ><span className="popover-link" data-reactroot=""><a href={`/profile/${author_img}`}>{author_name}</a></span></div>
-                            </div>
-
-                            <div className="container-fluid main-container">
-                    <div className="col-md-6 col-md-offset-1 dashboard-main-content">
-                        <div className="posts-wrapper animated fadeInUp" data-behavior="endless-scroll" data-animation="fadeInUp-fadeOutDown">
-                                         
-
-                        </div>
-                    </div>
-                </div>
-
-
-
-                              <button onClick={this.togglePopup.bind(this)}>Add Comment</button>
-{this.state.showPopup ? 
-          <Editor>
-                    </Editor>      
-          : null
-        }
-                        </div>
-                    </div>
-                </div>
-                </div>
-                </div>
+            </div>
         );
     }
 }
-ArticleView.propTypes = {
-    params: PropTypes.object.isRequired
-}
-export default connect(mapStateToProps, { 
-    getArticle,
-    clap,
-    follow
-})(ArticleView);
+
+ArticleView.propTypes = {params: PropTypes.object.isRequired}
+
+export default connect(mapStateToProps, {getArticle, clap, follow})(ArticleView);
